852bd04e20585bd0bcb195ef724d5e082a60921c
Sikender Shahid <sikendershahid91@gmail.com>
2017-08-02 04:22:42 -0500

    thread routines done

--- assignment3/plaza_management.c
+++ assignment3/plaza_management.c
@@ -1,47 +1,95 @@
 #include <stdio.h>
+#include <pthread.h>
 
 #define MAXCHILD 20
 
+static int capulet_count; 
+static int montague_count;
+static pthread_mutex_t access = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t ok = PTHREAD_COND_INITIALIZER;  
 
-struct Child{
+struct person{
 	char * family; 
 	char * name; 
 	unsigned int arrival_time; 
 	unsigned int stay_time; 
 };
 
-void create_thread(struct Child child){
-	
+void * capulet(void * arg){
+	struct person* a_person = (struct person *) arg;
+	printf("%s %s arrives at time %d\n", a_person->family, a_person->name, a_person->arrival_time); 
+
+	pthread_mutex_lock(&access); 
+	while(montague_count > 0){
+		pthread_cond_wait(&ok, &access); 
+	}
+	capulet_count++;
+	pthread_cond_signal(&ok); 
+	pthread_mutex_unlock(&access);  
+	printf("%s %s enters the plaza\n", a_person->family, a_person->name);
+	sleep(a_person->stay_time); // why cant i use this? 
+//	sleep(5); 
+	printf("%s %s leaves the plaza\n", a_person->family, a_person->name);
+	capulet_count--; 
+// why does ok get stuck if i dont broad cast? 
+	if(capulet_count == 0)
+		pthread_cond_broadcast(&ok); 
+	pthread_exit((void*)0); 
+
+}
+
+void * montague(void * arg){
+	struct person* a_person = (struct person *) arg;
+	printf("%s %s arrives at time %d\n", a_person->family, a_person->name, a_person->arrival_time); 
+
+	pthread_mutex_lock(&access); 
+	while(capulet_count > 0){
+		pthread_cond_wait(&ok, &access); 
+	}
+	montague_count++; 
+	pthread_cond_signal(&ok);
+	pthread_mutex_unlock(&access); 
+	printf("%s %s enters the plaza\n", a_person->family, a_person->name);
+	sleep(a_person->stay_time);  
+	printf("%s %s leaves the plaza\n", a_person->family, a_person->name);
+	montague_count--;
+	if(montague_count == 0)
+		pthread_cond_broadcast(&ok); 
+	pthread_exit((void*)0);   
 }
 
 int main(void){
 	char * buffer;
-	struct Child childs[MAXCHILD];
-	int child_count= 0;   
-	int arg_count = 0;  
+	struct person log[MAXCHILD];
+	int n_child= 0;   
+	int n_arg = 0;  
 
 	while(1){
 		scanf("%ms", &buffer);
 		if (buffer == NULL)
 			break;  
-		if (arg_count == 0) { childs[child_count].family = buffer;};
-		if (arg_count == 1) { childs[child_count].name = buffer;};
-		if (arg_count == 2) { childs[child_count].arrival_time = atoi(buffer);};
-		if (arg_count == 3) { 
-			childs[child_count].stay_time = atoi(buffer); 
-			arg_count = -1;
-			child_count++; 
+		if (n_arg == 0) { log[n_child].family = buffer;};
+		if (n_arg == 1) { log[n_child].name = buffer;};
+		if (n_arg == 2) { log[n_child].arrival_time = atoi(buffer);};
+		if (n_arg == 3) { 
+			log[n_child].stay_time = atoi(buffer); 
+			n_arg = -1; 
+			n_child++; 
 		};
-		arg_count++; 	
+		n_arg++; 	
 	} 
 
-	for(int i =0 ; i < child_count; i++){
-		printf("child#%d family:%s name:%s arrival_time:%d stay_time:%d \n", 
-			i,
-			childs[i].family, 
-			childs[i].name, 
-			childs[i].arrival_time, 
-			childs[i].stay_time); 
+	pthread_t tids[n_child];
+	void * (*gang) (void*);
+	for(int i = 0; i < n_child; i++){
+		gang = (strcmp(log[i].family, "Capulet") == 0)? capulet: montague;
+		pthread_create(&tids[i], NULL, gang, &log[i]);
+		sleep(1);  
 	} 
+
+	for(int i = 0; i < n_child; i++){
+		pthread_join(tids[i], NULL);  
+	}
+
 	return 0; 
 }

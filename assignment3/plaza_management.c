11edce51daaaf3ba3cdc100b8102fa0eb43ed9d9
Sikender Shahid <sikendershahid91@gmail.com>
2017-08-02 23:23:38 -0500

    Fixed timing errors

--- assignment3/plaza_management.c
+++ assignment3/plaza_management.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #define MAXCHILD 20
 
 static int capulet_count; 
 static int montague_count;
-static pthread_mutex_t access = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t ok = PTHREAD_COND_INITIALIZER;  
+static pthread_mutex_t lock_access = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t clear = PTHREAD_COND_INITIALIZER;  
 
 struct person{
 	char * family; 
@@ -18,43 +21,42 @@ struct person{
 void * capulet(void * arg){
 	struct person* a_person = (struct person *) arg;
 	printf("%s %s arrives at time %d\n", a_person->family, a_person->name, a_person->arrival_time); 
-
-	pthread_mutex_lock(&access); 
+	pthread_mutex_lock(&lock_access); 
 	while(montague_count > 0){
-		pthread_cond_wait(&ok, &access); 
+		pthread_cond_wait(&clear, &lock_access); 
 	}
 	capulet_count++;
-	pthread_cond_signal(&ok); 
-	pthread_mutex_unlock(&access);  
+	pthread_cond_signal(&clear);
 	printf("%s %s enters the plaza\n", a_person->family, a_person->name);
-	sleep(a_person->stay_time); // why cant i use this? 
-//	sleep(5); 
+	pthread_mutex_unlock(&lock_access); 
+	sleep(a_person->stay_time);
+	pthread_mutex_lock(&lock_access); 
 	printf("%s %s leaves the plaza\n", a_person->family, a_person->name);
-	capulet_count--; 
-// why does ok get stuck if i dont broad cast? 
+	capulet_count--;  
 	if(capulet_count == 0)
-		pthread_cond_broadcast(&ok); 
+		pthread_cond_broadcast(&clear);
+	pthread_mutex_unlock(&lock_access); 
 	pthread_exit((void*)0); 
-
 }
 
 void * montague(void * arg){
 	struct person* a_person = (struct person *) arg;
 	printf("%s %s arrives at time %d\n", a_person->family, a_person->name, a_person->arrival_time); 
-
-	pthread_mutex_lock(&access); 
+	pthread_mutex_lock(&lock_access); 
 	while(capulet_count > 0){
-		pthread_cond_wait(&ok, &access); 
+		pthread_cond_wait(&clear, &lock_access); 
 	}
 	montague_count++; 
-	pthread_cond_signal(&ok);
-	pthread_mutex_unlock(&access); 
+	pthread_cond_signal(&clear);
 	printf("%s %s enters the plaza\n", a_person->family, a_person->name);
-	sleep(a_person->stay_time);  
+	pthread_mutex_unlock(&lock_access); 
+	sleep(a_person->stay_time); 
+	pthread_mutex_lock(&lock_access); 
 	printf("%s %s leaves the plaza\n", a_person->family, a_person->name);
 	montague_count--;
 	if(montague_count == 0)
-		pthread_cond_broadcast(&ok); 
+		pthread_cond_broadcast(&clear);
+	pthread_mutex_unlock(&lock_access);  
 	pthread_exit((void*)0);   
 }
 
@@ -71,8 +73,7 @@ int main(void){
 		if (n_arg == 0) { log[n_child].family = buffer;};
 		if (n_arg == 1) { log[n_child].name = buffer;};
 		if (n_arg == 2) { log[n_child].arrival_time = atoi(buffer);};
-		if (n_arg == 3) { 
-			log[n_child].stay_time = atoi(buffer); 
+		if (n_arg == 3) { log[n_child].stay_time = atoi(buffer); 
 			n_arg = -1; 
 			n_child++; 
 		};
@@ -83,8 +84,9 @@ int main(void){
 	void * (*gang) (void*);
 	for(int i = 0; i < n_child; i++){
 		gang = (strcmp(log[i].family, "Capulet") == 0)? capulet: montague;
+		if(i!=0)
+			sleep(log[i].arrival_time - log[i-1].arrival_time); 
 		pthread_create(&tids[i], NULL, gang, &log[i]);
-		sleep(1);  
 	} 
 
 	for(int i = 0; i < n_child; i++){
